38645d939fa13158a2ff9b6d6cc14f5ebaa4c49e
SETIE LOUIS ADJAMBRI <setielouis.adjambri@ext.illimity.com>
2020-11-29 08:36:26 +0100

    INIT COMMIT +

---
+++ android/app/src/main/kotlin/com/github/spesa_sospesa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.spesa_sospesa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  runApp(
+      MaterialApp(
+        home: Scaffold(
+              body: Container(
+                child: Center(
+                  child: Text("Spesa sospesa"),
+                ),
+              ),
+          ),
+  ),
+  );
+}
+
