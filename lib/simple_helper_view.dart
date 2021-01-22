0e89009586377e849a2494211cc31239d864f783
SADJAMBR <SETIE-LOUIS.ADJAMBRI@consultant.vodafoneomnitel.it>
2021-01-22 09:30:56 +0100

    add getback btn

--- lib/simple_helper_view.dart
+++ lib/simple_helper_view.dart
@@ -32,66 +32,78 @@ class _SimpleHelperViewState extends State<SimpleHelperView> {
 
       return Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                flex: 2,
-                child: SafeArea(
-                  child: Padding(
-                    padding: EdgeInsets.all(5),
-                    child: Center(
-                        child: SvgPicture.asset(HOLD_HEART,
-                            height: 130, width: 130)
-                    ),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              // Navigator.canPop(context)
+
+              Container(
+                child: IconButton(
+                  icon: FaIcon(
+                    FontAwesomeIcons.times,
+                    size: 30,
+                    color: Colors.blueGrey[200],
                   ),
-                )
-            ),
-
-            Expanded(
-              flex: 6,
-              child: ListView.builder(
-                  itemCount: appSession.families.length,
-                  itemBuilder: (BuildContext context, int index){
-                    FamilyMap current = appSession.families[index];
-                    return   Container(
-                      margin: EdgeInsets.symmetric(vertical: 5 , horizontal: 10 ),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.grey[400].withOpacity(0.5),
-                            blurRadius: 2.0,
-                            spreadRadius: 1.0,
-                          ),
-                        ],
-                      ),
-                      child: FlatButton(
-
-                        onPressed: () {
-
-                          /// move deliver view
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                                return DeliverView(family: current);
-                              }));
-                        },
-                        child: ListTile(
-                          title: Text(current.family.name, style: TextStyle(
-                              fontSize: 14,
-                              fontFamily: "montserrat",
-                              fontWeight: FontWeight.w300,
-                              color: Colors.blueGrey[800]),
-                          ),
-                          trailing: getStateIcon( current.family.state),
-                        ),
-                      ),
-                    );
-                  }
+                  onPressed: () => Navigator.pop(context),
+                ),
               ),
-            )
+              Expanded(
+                  flex: 2,
+                  child: SafeArea(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 5),
+                      child: Center(
+                          child: SvgPicture.asset(HOLD_HEART,
+                              height: 130, width: 130)),
+                    ),
+                  )),
 
-          ],
+              Expanded(
+                flex: 6,
+                child: ListView.builder(
+                    itemCount: appSession.families.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      FamilyMap current = appSession.families[index];
+                      return Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey[400].withOpacity(0.5),
+                              blurRadius: 2.0,
+                              spreadRadius: 1.0,
+                            ),
+                          ],
+                        ),
+                        child: FlatButton(
+                          onPressed: () {
+                            /// move deliver view
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                              return DeliverView(family: current);
+                            }));
+                          },
+                          child: ListTile(
+                            title: Text(
+                              current.family.name,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontFamily: "montserrat",
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.blueGrey[800]),
+                            ),
+                            trailing: getStateIcon(current.family.state),
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
         ),
       );
     });
